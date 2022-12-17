8d5c2cae151548aad614b72c597f1fdea4cd0776
kouji2004 <mrtaxi48@gmail.com>
2022-12-17 00:04:57 +0000

    退会機能練習

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def unsubscribe
+  end
+
+  def withdrawal
+    @user = User.find(params[:id])
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
+  # 退会確認画面
+  get '/users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+  # 論理削除用のルーティング
+  patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
